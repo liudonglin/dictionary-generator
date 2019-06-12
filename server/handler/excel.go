eb71daf0f6fef7a2796b64d199ebd05e865d1568
liudonglin <454812428@qq.com>
2019-06-12 17:51:31 +0800

    update

--- server/handler/excel.go
+++ server/handler/excel.go
@@ -1,2 +1,22 @@
 package handler
 
+import (
+	"fmt"
+	"github.com/360EntSecGroup-Skylar/excelize/v2"
+)
+
+func testExcel() {
+    f := excelize.NewFile()
+    // Create a new sheet.
+    index := f.NewSheet("Sheet2")
+    // Set value of a cell.
+    f.SetCellValue("Sheet2", "A2", "Hello world.")
+    f.SetCellValue("Sheet1", "B2", 100)
+    // Set active sheet of the workbook.
+    f.SetActiveSheet(index)
+    // Save xlsx file by the given path.
+    err := f.SaveAs("./Book1.xlsx")
+    if err != nil {
+        fmt.Println(err)
+    }
+}
