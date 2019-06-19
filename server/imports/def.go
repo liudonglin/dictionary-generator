af3ae3ee2dc1c518abf31cbd86301d1d5eaf402b
liudonglin <454812428@qq.com>
2019-06-19 16:55:05 +0800

    update

--- server/imports/def.go
+++ server/imports/def.go
@@ -1,7 +1,6 @@
 package imports
 
 import (
-	"code-server/store/base/db"
 	"database/sql"
 )
 
@@ -38,80 +37,4 @@ type ColumnInfo struct {
 	NumericPrecision sql.NullString `json:"num_precision"`
 	//小数长度
 	NumericScale sql.NullString `json:"num_scale"`
-}
-
-func scanDB(scanner db.Scanner, dest *DBInfo) error {
-	return scanner.Scan(
-		&dest.DBName,
-	)
-}
-
-func scanDBs(rows *sql.Rows) ([]*DBInfo, error) {
-	defer rows.Close()
-
-	dbs := []*DBInfo{}
-	for rows.Next() {
-		db := new(DBInfo)
-		err := scanDB(rows, db)
-		if err != nil {
-			return nil, err
-		}
-		dbs = append(dbs, db)
-	}
-	return dbs, nil
-}
-
-func scanTable(scanner db.Scanner, dest *TableInfo) error {
-	return scanner.Scan(
-		&dest.TableName,
-		&dest.DBName,
-		&dest.Comment,
-	)
-}
-
-func scanTables(rows *sql.Rows) ([]*TableInfo, error) {
-	defer rows.Close()
-
-	tabs := []*TableInfo{}
-	for rows.Next() {
-		tab := new(TableInfo)
-		err := scanTable(rows, tab)
-		if err != nil {
-			return nil, err
-		}
-		tabs = append(tabs, tab)
-	}
-	return tabs, nil
-}
-
-func scanColumn(scanner db.Scanner, dest *ColumnInfo) error {
-	return scanner.Scan(
-		&dest.ColumnName,
-		&dest.TableName,
-		&dest.DBName,
-		&dest.Comment,
-		&dest.Null,
-		&dest.DataType,
-		&dest.ColumnType,
-		&dest.ColumnKey,
-		&dest.Extra,
-		&dest.CharacterMaximumLength,
-		&dest.NumericPrecision,
-		&dest.NumericScale,
-	)
-}
-
-func scanColumns(rows *sql.Rows) ([]*ColumnInfo, error) {
-	defer rows.Close()
-
-	cols := []*ColumnInfo{}
-	for rows.Next() {
-		col := new(ColumnInfo)
-		err := scanColumn(rows, col)
-		if err != nil {
-			return nil, err
-		}
-		cols = append(cols, col)
-	}
-	return cols, nil
 }
--- server/imports/idb.go
+++ server/imports/idb.go
@@ -2,12 +2,89 @@ package imports
 
 import (
 	"code-server/core"
+	"code-server/store/base/db"
 	"database/sql"
 	"errors"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
 )
+
+func scanDB(scanner db.Scanner, dest *DBInfo) error {
+	return scanner.Scan(
+		&dest.DBName,
+	)
+}
+
+func scanDBs(rows *sql.Rows) ([]*DBInfo, error) {
+	defer rows.Close()
+
+	dbs := []*DBInfo{}
+	for rows.Next() {
+		db := new(DBInfo)
+		err := scanDB(rows, db)
+		if err != nil {
+			return nil, err
+		}
+		dbs = append(dbs, db)
+	}
+	return dbs, nil
+}
+
+func scanTable(scanner db.Scanner, dest *TableInfo) error {
+	return scanner.Scan(
+		&dest.TableName,
+		&dest.DBName,
+		&dest.Comment,
+	)
+}
+
+func scanTables(rows *sql.Rows) ([]*TableInfo, error) {
+	defer rows.Close()
+
+	tabs := []*TableInfo{}
+	for rows.Next() {
+		tab := new(TableInfo)
+		err := scanTable(rows, tab)
+		if err != nil {
+			return nil, err
+		}
+		tabs = append(tabs, tab)
+	}
+	return tabs, nil
+}
+
+func scanColumn(scanner db.Scanner, dest *ColumnInfo) error {
+	return scanner.Scan(
+		&dest.ColumnName,
+		&dest.TableName,
+		&dest.DBName,
+		&dest.Comment,
+		&dest.Null,
+		&dest.DataType,
+		&dest.ColumnType,
+		&dest.ColumnKey,
+		&dest.Extra,
+		&dest.CharacterMaximumLength,
+		&dest.NumericPrecision,
+		&dest.NumericScale,
+	)
+}
+
+func scanColumns(rows *sql.Rows) ([]*ColumnInfo, error) {
+	defer rows.Close()
+
+	cols := []*ColumnInfo{}
+	for rows.Next() {
+		col := new(ColumnInfo)
+		err := scanColumn(rows, col)
+		if err != nil {
+			return nil, err
+		}
+		cols = append(cols, col)
+	}
+	return cols, nil
+}
 
 func getConn(info *core.Connection) (*sqlx.DB, error) {
 
